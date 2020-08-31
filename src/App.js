c5b8bb731987fcf08104c743a794fa26f0eb5632
margare-jihua <margaret.jihua@gmail.com>
2020-08-30 22:46:48 -0700

    Comment Errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './style.css'
+import axios from 'axios'
 
 import {
   BrowserRouter as Router,
@@ -7,15 +8,55 @@ import {
   Link
 }from 'react-router-dom'
 
-import Home from './Home'
+import StarshipPage from './StarshipPage'
 
 function App() {
+  const [starships, SetStarships] = useState('')
+
+  useEffect (() => {      
+    let url = 'https://swapi.dev/api/starships'
+    axios.get(url)
+    .then(res => {
+      // console.log(res.data);
+      SetStarships(res.data.results)
+      // console.log(starships);
+    })
+    .catch(err => console.log(err))
+  })
+
+  // THIS SAYS TypeERROR "MAP IS NOT A FUNCTION"
+
+  // let starshipLinks = starships.map(starship => {
+  //   return(
+  //    <Link
+  //     to={{
+  //       pathname: '/starship',
+  //       state: starship
+  //     }}
+  //     key={starship.name}
+  //   >
+  //     {starship.name}
+  //   </Link>
+  //     )})
+
+console.log(starships);// THIS GETS CONTINUOUS CALLS
+console.log(starships[0].name);// THIS GIVES UNDEFINE
+
   return (
     <Router>
       <nav>
         STAR WARS STARSHIPS
       </nav>
-      <Route exact path='/' component={Home} />
+      {/* THIS SAYS TypeERROR "MAP IS NOT A FUNCTION" */}
+
+      {/* {starships.map((starship,i) => (
+        <Link>{starship[0]}</Link>
+      ))} */}
+      {/* <div><Link to='/starship'>{starships[0].name}</Link></div> */}
+      {/* <div>{starshipLinks}</div> */}
+      <Route path='/starship' render={({location}) => 
+                  <StarshipPage location={location}/>
+      }/>
     </Router>
   );
 }
---
+++ src/StarshipPage.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+export default class StarshipPage extends Component {
+    render() {
+        return (
+            <div className="box">
+                <p>NAME: </p>
+                <p>MODEL: </p>
+                <a href='/'><p>Return</p></a>
+            </div>
+        )
+    }
+}
+
